3a876309d0b2373a247a1afe487def778725c579
GoodStuff11 <34872749+GoodStuff11@users.noreply.github.com>
2019-03-18 00:17:33 -0400

    Update DataFrame.h

--- DataFrame.h
+++ DataFrame.h
@@ -1,22 +1,29 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
-#include<string>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include "list.h"
+
 using namespace std;
 
 class DataFrame {
 private:
-	int ncol;
+	int ncol = 0;
 	string *colTitle;
-	vector<double*>*row;
+	list<double*>*row;
 
-	string* splitString(string str,char delimiter);
-	double* splitNum(string str,char delimiter);
+	string* splitString(string str, char delimiter);
+	double* splitNum(string str, char delimiter);
+
+	int partition(list<double*>*L,int lo,int hi,int col);
+	void quicksort(list<double*>*L,int lo,int hi,int col);
 public:
-	DataFrame(char* address,char delimiter);
+	DataFrame(const char* address, char delimiter);
 	virtual ~DataFrame();
-
 	void disp();
+	void sort(int col);
+
 };
