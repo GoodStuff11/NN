34f06e4ceb218058e38d667f5e9d6206d6c1bdee
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-20 22:00:33 -0400

    Update list.cpp

--- data types/list.cpp
+++ data types/list.cpp
@@ -3,7 +3,6 @@
 template<class t>
 list<t>::list() {
 	length = 0;
-	//I dont know why it needs this
 	first = nullptr;
 	last = nullptr;
 	current = nullptr;
@@ -26,10 +25,6 @@ int list<t>::size() {
 	return length;
 }
 template<class t>
-int list<t>::size() const {
-	return length;
-}
-template<class t>
 void list<t>::push_back(t val) {
 	if (first == nullptr) {
 		first = new element<t> { val };
@@ -44,72 +39,6 @@ void list<t>::push_back(t val) {
 	}
 	length++;
 }
-template<class t>
-void list<t>::swap(int i1, int i2) {
-	if (i1 == i2)
-		return;
-	if (i2 < i1) {
-		int i = i2;
-		i2 = i1;
-		i1 = i;
-	}
-	if (i1 == 0 && i2 != size() - 1) {
-		go(0);
-		element<t>*e1 = current;
-
-		go(i2 - 1);
-		element<t>*e2 = current;
-
-		element<t>*temp = e1->next;
-		e1->next = e2->next->next;
-		e2->next->next = temp;
-
-		first = e2->next;
-		e2->next = e1;
-	} else if (i2 == size() - 1 && i1 != 0) {
-		go(i1 - 1);
-		element<t>*e1 = current;
-
-		go(i2 - 1);
-		element<t>*e2 = current;
-
-		last = e1->next;
-		e2->next->next = e1->next->next;
-
-		element<t>*temp = e1->next;
-		e1->next = e2->next;
-		e2->next = temp;
-
-
-	} else if (i1 == 0) {
-		element<t>*temp = first;
-		first = last;
-		last = temp;
-
-		go(0);
-		element<t>*e1 = current;
-
-		go(i2 - 1);
-		element<t>*e2 = current;
-
-		e2->next->next = e1->next;
-		e2->next = e1;
-	} else {
-		go(i1 - 1);
-		element<t>*e1 = current;
-
-		go(i2 - 1);
-		element<t>*e2 = current;
-
-		element<t>*temp = e1->next->next;
-		e1->next->next = e2->next->next;
-		e2->next->next = temp;
-
-		element<t>*temp2 = e1->next;
-		e1->next = e2->next;
-		e2->next = temp2;
-	}
-}
 
 template<class t>
 t* list<t>::refAt(int index) {
@@ -122,19 +51,6 @@ t list<t>::valAt(int index) {
 	go(index);
 	return current->val;
 }
-template<class t>
-t list<t>::valAt(int index) const {
-	if (index < cindex) {
-		current = first;
-		cindex = 0;
-	}
-	for (int i = cindex; i < index; i++) {
-		cindex = cindex + 1;
-		current = current->next;
-	}
-	return current->val;
-}
-//TODO: debug special cases
 template<class t>
 void list<t>::erase(int index) {
 	element<t> *temp = first;
