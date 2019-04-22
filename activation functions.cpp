41542c168a1e5044492e5fba3a9751a063a0e361
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-22 17:13:32 -0400

    Update activation functions.cpp

--- activation functions.cpp
+++ activation functions.cpp
@@ -1,68 +1,86 @@
 #include "activation functions.h"
 
-void softmax(Matrix *mat){
+Matrix softmax::call(Matrix mat){
 	double sum = 0;
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->columns;j++)
-			sum += (*mat)(i,j);
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->columns;j++)
-			(*mat)(i,j) /= sum;
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.columns;j++)
+			sum += exp(mat(i,j));
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.columns;j++)
+			mat(i,j) = exp(mat(i,j))*sum;
+	return mat;
 }
-void sigmoid(Matrix *mat){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++)
-			(*mat)(i,j) = 1/(exp(-(*mat)(i,j)) + 1);
+Matrix softmax::call_derivative(Matrix mat){
+	double sum = 0;
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.columns;j++)
+			sum += exp(mat(i,j));
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.columns;j++)
+			mat(i,j) = exp(mat(i,j))*sum;
+	return mat;
 }
-void softplus(Matrix *mat){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++)
-			(*mat)(i,j) = log(1+exp((*mat)(i,j)));
+Matrix sigmoid::call(Matrix mat){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++)
+			mat(i,j) = 1/(exp(-mat(i,j)) + 1);
+	return mat;
 }
-void tanh(Matrix *mat){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++)
-			(*mat)(i,j) = tanh((*mat)(i,j));
+Matrix softplus::call(Matrix mat){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++)
+			mat(i,j) = log(1+exp(mat(i,j)));
+	return mat;
 }
-void relu(Matrix *mat){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++){
-			double val = (*mat)(i,j);
+Matrix tanh::call(Matrix mat){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++)
+			mat(i,j) = tanh(mat(i,j));
+	return mat;
+}
+Matrix relu::call(Matrix mat){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++){
+			double val = mat(i,j);
 			if (val<0)
-				(*mat)(i,j) = 0;
+				mat(i,j) = 0;
 			else
-				(*mat)(i,j) = val;
+				mat(i,j) = val;
 		}
+	return mat;
 }
-void elu(Matrix *mat,double a){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++){
-			double val = (*mat)(i,j);
+Matrix elu::call(Matrix mat,double a){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++){
+			double val = mat(i,j);
 			if (val<0)
-				(*mat)(i,j) = a*(exp(val) - 1);
+				mat(i,j) = a*(exp(val) - 1);
 			else
-				(*mat)(i,j) = val;
+				mat(i,j) = val;
 		}
+	return mat;
 }
-void leaky_relu(Matrix *mat,double a){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++){
-			double val = (*mat)(i,j);
+Matrix leaky_relu::call(Matrix mat,double a){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++){
+			double val = mat(i,j);
 			if (val<0)
-				(*mat)(i,j) = a*val;
+				mat(i,j) = a*val;
 			else
-				(*mat)(i,j) = val;
+				mat(i,j) = val;
 		}
+	return mat;
 }
-void step(Matrix *mat){
-	for (unsigned int i=0;i<mat->rows;i++)
-		for (unsigned int j=0;j<mat->rows;j++){
-			if ((*mat)(i,j)<0)
-				(*mat)(i,j) = 0;
+Matrix step::call(Matrix mat){
+	for (unsigned int i=0;i<mat.rows;i++)
+		for (unsigned int j=0;j<mat.rows;j++){
+			if (mat(i,j)<0)
+				mat(i,j) = 0;
 			else
-				(*mat)(i,j) = 1;
+				mat(i,j) = 1;
 		}
+	return mat;
 }
-void none(Matrix *mat){
-	return;
+Matrix none::call(Matrix mat){
+	return mat;
 }
