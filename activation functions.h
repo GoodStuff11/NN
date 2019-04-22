23bbd2124a76a6359e3885ca7da8a4b4a60acc7a
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-22 17:13:15 -0400

    converting activation functions into classes

--- activation functions.h
+++ activation functions.h
@@ -1,16 +1,35 @@
 #ifndef ACTIVATION_FUNCTIONS_H_
 #define ACTIVATION_FUNCTIONS_H_
-#include "Matrix.h"
+#include "data types/Matrix.h"
 #include <cmath>
+class activation {
+public:
+	virtual Matrix call(Matrix mat) {
+		return mat;
+	}
+	virtual Matrix call_derivative(Matrix mat) {
+		return Matrix(mat.rows, mat.columns);
+	}
+};
+class softmax: public activation {
+public:
+	Matrix call(Matrix mat);
+	Matrix call_derivative(Matrix mat);
+};
+class sigmoid: public activation {
+public:
+	Matrix call(Matrix mat);
+	Matrix call_derivative(Matrix mat);
+};
 
-void softmax(Matrix *mat);
-void sigmoid(Matrix *mat);
-void relu(Matrix *mat);
-void leaky_relu(Matrix *mat,double a);
-void tanh(Matrix *mat);
-void elu(Matrix *mat,double a);
-void step(Matrix *mat);
-void softplus(Matrix *mat);
-void none(Matrix *mat);
+Matrix softmax(Matrix mat);
+Matrix sigmoid(Matrix mat);
+Matrix relu(Matrix mat);
+Matrix leaky_relu(Matrix mat, double a);
+Matrix tanh(Matrix mat);
+Matrix elu(Matrix mat, double a);
+Matrix step(Matrix mat);
+Matrix softplus(Matrix mat);
+Matrix none(Matrix mat);
 
 #endif /* ACTIVATION_FUNCTIONS_H_ */
