00395838798cda86e74617219a2388cb60141026
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-05-07 00:35:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "data types/DataFrame.h"
 #include "data types/Matrix.h"
-#include "NeuralNetwork.h"
+//#include "NeuralNetwork.h"
 #include <iostream>
 #include <stdlib.h>
 
@@ -24,10 +24,33 @@ int main() {
 	df->sort_by(0);
 	df->print();
 	cout << endl;
-	*/
+
 	NeuralNetwork *model = new NeuralNetwork();
 	model->add_layer(new dense(2,2,relu));
 	model->add_layer(new dense(2,1,softmax));
 
+	Matrix m = Matrix(2,1);
+	m(0)=1;
+	m(1)=2;
+	model->predict(m).print();
+	cout << endl;
+*/
+	Matrix mat = Matrix(3,3);
+	mat(0,1) = 1;
+	mat(1,0) = 1;
+	mat(2,2) = 1;
+	mat.print();
+
+	cout<< endl;
+
+	Vector v = Vector(3);
+	v(0) = 2;
+	v(1) = 3;
+	v(2) = 8;
+	v.print();
+
+	cout << endl;
+	v = mat*v;
+	v.transpose().print();
 	return 0;
 }
