10e062d12754d11b7d2943537a80d2ccef41cb77
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-05-07 00:35:36 -0400

    Update Matrix.cpp

--- data types/Matrix.cpp
+++ data types/Matrix.cpp
@@ -1,36 +1,25 @@
 #include "Matrix.h"
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-Matrix::Matrix() {
-	return;
+MatrixStructure::MatrixStructure() {
+	transposed = false;
+	rows = 0;
+	columns = 0;
 }
-Matrix::Matrix(unsigned int rows, unsigned int columns) {
+MatrixStructure::MatrixStructure(unsigned int rows, unsigned int columns) {
+	transposed = false;
 	this->columns = columns;
 	this->rows = rows;
 
-	matrix.resize(rows * columns);
+	array.resize(rows * columns);
 	for (unsigned int i = 0; i < rows * columns; i++)
-		matrix[i] = 0;
+		array[i] = 0;
 }
 
-double& Matrix::operator()(unsigned int row, unsigned int column) {
-	return matrix[columns * row + column];
-}
-double& Matrix::operator()(unsigned int row) {
-	return matrix[row * columns];
-}
-Matrix Matrix::transpose() {
-	Matrix new_matrix = Matrix(columns, rows);
-	for (unsigned int i = 0; i < rows; i++) {
-		for (unsigned int j = 0; j < columns; j++) {
-			new_matrix(j, i) = (*this)(i, j);
-		}
-	}
-	return new_matrix;
-}
-void Matrix::print() {
+void MatrixStructure::print() {
 	for (unsigned int i = 0; i < rows; i++) {
 		for (unsigned int j = 0; j < columns; j++) {
 			cout << (*this)(i, j) << ' ';
@@ -39,38 +28,85 @@ void Matrix::print() {
 	}
 }
 
-Matrix Matrix::operator+(Matrix other) {
-	Matrix new_matrix = Matrix(rows, columns);
+MatrixStructure MatrixStructure::operator+(MatrixStructure other) {
+	MatrixStructure new_array = MatrixStructure(rows, columns);
 	for (unsigned int i = 0; i < rows * columns; i++)
-		new_matrix.matrix[i] = this->matrix[i] + other.matrix[i];
-	return new_matrix;
-}
-Matrix Matrix::operator=(Matrix other) {
-	matrix = other.matrix;
-	columns = other.columns;
-	rows = other.rows;
-	return *this;
+		new_array.array[i] = this->array[i] + other.array[i];
+	return new_array;
 }
 
-Matrix Matrix::operator-(Matrix other) {
-	Matrix new_matrix = Matrix(rows, columns);
+MatrixStructure MatrixStructure::operator-(MatrixStructure other) {
+	MatrixStructure new_array = MatrixStructure(rows, columns);
 	for (unsigned int i = 0; i < rows * columns; i++)
-		new_matrix.matrix[i] = this->matrix[i] - other.matrix[i];
-	return new_matrix;
+		new_array.array[i] = this->array[i] - other.array[i];
+	return new_array;
 }
 
-Matrix Matrix::operator*(Matrix other) {
-	Matrix new_matrix = Matrix(rows, other.columns);
+MatrixStructure MatrixStructure::operator*(MatrixStructure other) {
+	MatrixStructure new_array = MatrixStructure(rows, other.columns);
 	for (unsigned int i = 0; i < rows; i++) {
 		for (unsigned int j = 0; j < other.columns; j++) {
 			double sum = 0;
 			for (unsigned int k = 0; k < columns; k++) {
 				sum += (*this)(i, k) * other(k, j);
 			}
-			new_matrix(i, j) = sum;
+			new_array(i, j) = sum;
 		}
 	}
-	return new_matrix;
+	return new_array;
+}
+MatrixStructure MatrixStructure::transpose() {
+	transposed = true;
+
+	unsigned int temp = rows;
+	rows = columns;
+	columns = temp;
+
+	return *this;
+}
+
+Matrix::Matrix() {
+	rows = 0;
+	columns = 0;
+	return;
+}
+Matrix Matrix::operator=(MatrixStructure other) {
+	transposed = MatrixStructure::transposed;
+	array = MatrixStructure::array;
+	columns = MatrixStructure::columns;
+	rows = MatrixStructure::rows;
+	return *this;
+}
+unsigned int Matrix::get_rows() {
+	//if (transposed)
+	//	return columns;
+	return rows;
+}
+unsigned int Matrix::get_columns() {
+	//if (transposed)
+	//	return rows;
+	return columns;
+}
+double& MatrixStructure::operator()(unsigned int row, unsigned int column) {
+	//http://www.cplusplus.com/reference/string/to_string/
+	//https://stackoverflow.com/questions/12261915/how-to-throw-stdexceptions-with-variable-messages
+	if (transposed) {
+		unsigned int temp = row;
+		row = column;
+		column = temp;
+	}
+	if (row >= rows)
+		throw runtime_error(
+				"IndexError: " + to_string(row) + " >= " + to_string(rows));
+	if (column >= columns)
+		throw runtime_error(
+				"IndexError: " + to_string(column) + " >= "
+						+ to_string(columns));
+	return array[columns * row + column];
+}
+Matrix::Matrix(unsigned int rows, unsigned int columns) :
+		MatrixStructure(rows, columns) {
+	return;
 }
 
 IdentityMatrix::IdentityMatrix(unsigned int size) :
@@ -78,8 +114,39 @@ IdentityMatrix::IdentityMatrix(unsigned int size) :
 	for (unsigned int i = 0; i < size; i++)
 		(*this)(i, i) = 1;
 }
+
+Vector::Vector() {
+	rows = 0;
+	columns = 0;
+}
+Vector Vector::operator=(MatrixStructure other) {
+	//https://en.cppreference.com/w/cpp/language/access
+	//kinda inspired by this, but i mostly just guessed and it worked
+	transposed = MatrixStructure::transposed;
+	array = MatrixStructure::array;
+	columns = MatrixStructure::columns;
+	rows = MatrixStructure::rows;
+	return *this;
+}
+
+double& Vector::operator()(unsigned int index) {
+	if (index >= get_size())
+		throw runtime_error(
+				"IndexError: " + to_string(index) + " >= "
+						+ to_string(get_size()));
+	return array[index];
+}
+
+double& Vector::operator()(unsigned int row, unsigned int column) {
+	return MatrixStructure::operator()(row, column);
+}
 Vector::Vector(unsigned int size) :
-		Matrix(size, 1) {
+		MatrixStructure(size, 1) {
 	for (unsigned int i = 0; i < size; i++)
-		(*this)(i) = 1;
+		(*this)(i) = 0;
+}
+unsigned int Vector::get_size() {
+	if (columns != 1)
+		return columns;
+	return rows;
 }
