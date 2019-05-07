2a38bfebebdcfa137cf8a788546bd89ae0e2e979
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-05-07 00:35:20 -0400

    transpose doesnt work

--- data types/Matrix.h
+++ data types/Matrix.h
@@ -2,29 +2,37 @@
 #define MATRIX_H_
 #include <vector>
 class MatrixStructure{
+protected:
+	std::vector<double> array;
+	unsigned int rows;
+	unsigned int columns;
+	bool transposed;
+
+	double& operator()(unsigned int row, unsigned int column);
+	MatrixStructure(unsigned int rows, unsigned int columns);
+	MatrixStructure();
 public:
-	protected std::vector<double> array;
-	protected unsigned int rows;
-	protected unsigned int columns;
 
 	MatrixStructure operator+(MatrixStructure other);
 	MatrixStructure operator*(MatrixStructure other);
 	MatrixStructure operator-(MatrixStructure other);
-	MatrixStructure operator=(MatrixStructure other);
 	MatrixStructure operator==(MatrixStructure other);
 	MatrixStructure transpose();
 	void print();
-}
+};
 class Matrix: public MatrixStructure{
-private:
-	std::vector<double> matrix;
 public:
+	//https://stackoverflow.com/questions/2986891/how-to-publicly-inherit-from-a-base-class-but-make-some-of-public-methods-from-t
+
 	Matrix();
 	Matrix(unsigned int rows, unsigned int columns);
-	
-	int get_rows();
-	int get_columns();
-	double& operator()(unsigned int row, unsigned int column);
+
+	unsigned int get_rows();
+	unsigned int get_columns();
+
+	Matrix operator=(MatrixStructure other);
+	//double& operator()(unsigned int row, unsigned int column);
+	using MatrixStructure::operator();
 };
 
 //https://stackoverflow.com/questions/120876/what-are-the-rules-for-calling-the-superclass-constructor
@@ -32,12 +40,16 @@ class IdentityMatrix: public Matrix {
 public:
 	IdentityMatrix(unsigned int size);
 };
+
 class Vector: public MatrixStructure {
+protected:
+	double& operator()(unsigned int row, unsigned int column);
 public:
 	Vector();
 	Vector(unsigned int size);
-	
-	int get_size();
+
+	Vector operator=(MatrixStructure other);
+	unsigned int get_size();
 	double& operator()(unsigned int index);
 };
 
