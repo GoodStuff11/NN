295c5ebc4770e630c88cc54d3bad8e493e8e2357
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-05-10 10:51:46 -0400

    reworked activation functions

--- activation functions.h
+++ activation functions.h
@@ -2,61 +2,17 @@
 #define ACTIVATION_FUNCTIONS_H_
 
 #include "data types/Matrix.h"
+#include <string>
 class activation {
+private:
+	string function;
+	double constant;
 public:
-	virtual Vector call(Vector v) {
-		return v;
-	}
-	virtual Matrix call_derivative(Vector v) {
-		return IdentityMatrix(v.get_size());
-	}
-};
-class softmax: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class sigmoid: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class relu: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class leaky_relu: public activation {
-public:
-	//assume a = 0.5
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class tanh: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class elu: public activation {
-public:
-	//assume a = 0.5
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class step: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class softplus: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
-};
-class none: public activation {
-public:
-	Vector call(Vector v);
-	Matrix call_derivative(Vector v);
+	activation(string function);
+	activation(string function,double constant);
+			
+	Matrix call_derivative(Vector input);
+	Vector call(Vector input);
 };
 
 #endif /* ACTIVATION_FUNCTIONS_H_ */
